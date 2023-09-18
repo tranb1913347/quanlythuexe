fa38651846b262328d169c0f8c8fb5f6f974d9b4
John Smith <vanman12315@gmail.com>
2023-09-19 00:33:24 +0700

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express")
+const express =  require("express")
 const app = express();
 const path = require("path");
 const {sequelize} = require("./models")
 const {rootRouter} = require("./Routers")
 const cors = require("cors")
-
+//heloo
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
